7140486cad5dc707f0c6e8ffbfdab04eb398a921
Umidjon Amirqulov <uamirqulov5@gmail.com>
2025-04-13 22:39:50 +0500

    add couter page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Hero } from './Hero'
 import { About } from '../Pages/About/About'
 import { Services } from '../Pages/Services/Services'
+import { Counter } from '../Pages/Counter/Counter'
 export const Home = () => {
   return (
     <>
     <Hero />
     <About />
     <Services />
+    <Counter />
     </>
   )
 }
---
+++ src/components/Pages/Counter/Counter.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import './Counter.scss'
+import { project } from '../../Data/mydata'
+
+export const Counter = () => {
+  return (
+    <>
+    <div className="hero counter">
+        <div className="container grid3 grid4">
+            {project.map((item, i)=>(
+                <div className="box">
+                    <i>{item.icon}</i>
+                    <h1 className='heading'>{item.num}</h1>
+                    <h3>{item.title}</h3>
+                </div>
+            ))}
+        </div>
+    </div>
+    </>
+  )
+}
